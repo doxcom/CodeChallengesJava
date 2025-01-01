c158387753a7bab20263b1a77de4886733dcbc35
doxcom <aldoperez1319@gmail.com>
2024-12-31 21:02:24 -0600

    initial code for issue

---
+++ LongestCommonPrefixSolution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefixSolution {
+
+    public static void main(String[] args) {
+
+        LongestCommonPrefixSolution lcp = new LongestCommonPrefixSolution();
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(lcp.longestCommonPrefix(strs));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+
+        char[] prefix = new char[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+
+        }
+
+        Arrays.toString(prefix);
+
+        return prefix;
+    }
+
+}
